a0488e22e1f6e4d20be2583451ac03cafe8f1864
Kaushal Kushwaha <93634851+kaushalleng@users.noreply.github.com>
2023-12-14 17:28:56 +0000

    datatypes

---
+++ 01_basics/03_dataTypes.js
@@ -0,0 +1,40 @@
+"use strict"; //treat all JS code as a newer version
+
+// aleart(2+3)  //we are using nodeJS, not browser
+
+// console.log(5 
+//     + 2); //code readablity should be high
+
+// console.log("Enterprises")
+
+let name = "Moye"
+let age = 19
+let isLoggedIn = true  //false
+
+
+                     /*Primitive Data Types*/
+
+// Number => -(253 − 1) (Number.MIN_SAFE_INTEGER) to 253 − 1 (Number.MAX_SAFE_INTEGER) // "2 to pwr 53"
+// BigInt =>
+// string => ""
+// boolean => true/false
+
+// null => indicates an intentional absence = जानबूझकर अनुपस्थिति of an object value // standalone value
+// undefined => indicates the absence of a value 
+// console.log(null === undefined)
+
+// symbol => unique may be used as the key "like of an Object property"
+
+
+                    /*non Primitive Data Type*/
+
+// object => [keys = values]
+
+
+// typeof keyword => returns a string indicating the type of the operand's value.
+// console.log(typeof "we are just start learning java-script") // string
+// console.log(typeof 19) // number
+// console.log(typeof true) // boolean
+
+console.log(typeof null) // *object
+console.log(typeof undefined) // *undefined
